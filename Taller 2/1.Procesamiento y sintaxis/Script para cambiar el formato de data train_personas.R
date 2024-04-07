017aef2ad8a2467957e6a8afda9ebf9a0b83a397
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-04-07 10:48:18 -0500

    Revert "Subir data train_personas en formato R"
    This reverts commit 702837d2576bb5c6d0bc1cc5c35f9dcb044e6cf6.

--- Taller 2/1.Procesamiento y sintaxis/Script para cambiar el formato de data train_personas.R
+++
@@ -1,8 +0,0 @@
-#Este script convierte la data de train_personas en formato .R
-#ya que es un archivo muy grande para ser cargado en github como .csv
-
-#importamos la data descargada localmente
-
-train_personas <- read.csv("C:/Users/HP-Laptop/Desktop/train_personas.csv")
-
-save(train_personas,file = "train_personas.R")
