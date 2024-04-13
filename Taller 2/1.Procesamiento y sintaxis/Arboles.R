bfd857dd99193241481cb37783641eaad18d909c
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-04-12 22:35:28 -0500

    Update de scripts y submission

--- Taller 2/1.Procesamiento y sintaxis/Arboles.R
+++ Taller 2/1.Procesamiento y sintaxis/Arboles.R
@@ -12,9 +12,37 @@ source_url("https://raw.githubusercontent.com/Esteban7777/Curso-Big-Data/main/Ta
 train_hogares$pobre_texto<-ifelse(train_hogares$Pobre==1,"Pobre","No_Pobre")
 train_hogares$pobre_texto<-as.factor(train_hogares$pobre_texto)
 
+
+train_hogares$jefe_joven<-ifelse(train_hogares$edad_jefe<=28,"Joven","No_Joven")
+train_hogares$jefe_joven<-as.factor(train_hogares$jefe_joven)
+test_hogares$jefe_joven<-ifelse(test_hogares$edad_jefe<=28,"Joven","No_Joven")
+test_hogares$jefe_joven<-as.factor(test_hogares$jefe_joven) 
+
+train_hogares$jefe_menor<-ifelse(train_hogares$edad_jefe<18,"Menor","No_Menor")
+train_hogares$jefe_menor<-as.factor(train_hogares$jefe_menor)
+test_hogares$jefe_menor<-ifelse(test_hogares$edad_jefe<18,"Menor","No_Menor")
+test_hogares$jefe_menor<-as.factor(test_hogares$jefe_menor) 
+
+
+train_hogares$Personas_habitacion_round<-ifelse(train_hogares$Personas_habitacion<=2.5,1,0)
+train_hogares$Personas_habitacion_round<-as.factor(train_hogares$Personas_habitacion_round)
+
+test_hogares$Personas_habitacion_round<-ifelse(test_hogares$Personas_habitacion<=2.5,1,0)
+test_hogares$Personas_habitacion_round<-as.factor(test_hogares$Personas_habitacion_round)
+
+train_hogares$rural<-ifelse(train_hogares$Dominio=="RURAL",1,0)
+test_hogares$rural<-ifelse(test_hogares$Dominio=="RURAL",1,0)
+train_hogares$rural<-as.factor(train_hogares$rural)
+test_hogares$rural<-as.factor(test_hogares$rural)
+table(train_hogares$rural,train_hogares$Dominio)
+table(test_hogares$rural,test_hogares$Dominio)
+
 predictores_modelo<-c("educacion_jefe",
+                      "tipo_casa",
+                      "jefe_joven",
+                      "jefe_joven",
                        "P5090",
-                       "Personas_habitacion",
+                       "Personas_habitacion_round",
                        "Des_jefe",
                        "sexo_jefe",
                        "Clase")
@@ -22,8 +50,8 @@ predictores_modelo<-c("educacion_jefe",
 arbol1<-rpart(formula = as.formula(paste("pobre_texto~",
         paste(predictores_modelo, collapse = " + "))),
         data=train_hogares,
-        method = "class",
-        parms = list(split="Gini"))
+        method = "class"
+        )
 prp(arbol1)
 
 train_hogares$predic_arbol1<-predict(arbol1,newdata =train_hogares,type = "class")
@@ -36,8 +64,8 @@ test_hogares$predic_arbol1<-predict(arbol1,newdata =test_hogares,type = "class")
 table(test_hogares$predic_arbol1)
 
 predictores_modelo<-c("posicion_jefe",
-                      "Ina_jefe",
-                      "Des_jefe",
+                      "Ina_jefe","tipo_casa",
+                      "Des_jefe","jefe_joven",
                       "Oc_jefe",
                       "Pet_jefe",
                       "ocupacion_jefe",
@@ -45,7 +73,7 @@ predictores_modelo<-c("posicion_jefe",
                       "regimen_jefe",
                       "sexo_jefe",
                       "Clase",
-                      "Dominio",
+                      #"Dominio",
                       "P5000",
                       "P5010",
                       "P5090",
@@ -57,28 +85,133 @@ predictores_modelo<-c("posicion_jefe",
                       "Depto",                  
                       "regimen_subsidiado_jefe",
                       "desempleo_jefe",
-                      "Personas_habitacion"
-  
-)
+                      "Personas_habitacion_round"
+  )
 
+
+train_hogares$Depto<-as.factor(train_hogares$Depto)
+test_hogares$Depto<-as.factor(test_hogares$Depto)
 train_hogares$regimen_jefe<-as.factor(train_hogares$regimen_jefe)
 train_hogares$posicion_jefe<-as.factor(train_hogares$posicion_jefe)
 train_hogares$ocupacion_jefe<-as.factor(train_hogares$ocupacion_jefe)
 test_hogares$ocupacion_jefe<-as.factor(test_hogares$ocupacion_jefe)
+
+train_hogares_sin_bogota<-train_hogares %>% filter(Dominio!="BOGOTA")
+
 arbol2<-rpart(formula = as.formula(paste("pobre_texto~",
                                          paste(predictores_modelo, 
                                                collapse = " + "))),
-              data=train_hogares,
+              data=train_hogares_sin_bogota,
               method = "class",
               parms = list(split="Gini"))
 
 prp(arbol2)
 
-train_hogares$predic_arbol2<-predict(arbol2,newdata =train_hogares,type = "class")
+train_hogares_sin_bogota$predic_arbol2<-predict(arbol2,newdata =train_hogares_sin_bogota,type = "class")
 
-confusionMatrix(data = train_hogares$predic_arbol2,
-                reference = train_hogares$pobre_texto,
+confusionMatrix(data = train_hogares_sin_bogota$predic_arbol2,
+                reference = train_hogares_sin_bogota$pobre_texto,
                 positive="Pobre", mode = "prec_recall")
 
 test_hogares$predic_arbol2<-predict(arbol2,newdata =test_hogares,type = "class")
 table(test_hogares$predic_arbol2)
+
+sub9<-test_hogares %>% select(id,predic_arbol2)
+sub9<-sub9 %>% rename(pobre=predic_arbol2)
+sub9$pobre<-ifelse(sub9$pobre=="Pobre",1,0)
+table(test_hogares$predic_arbol2)
+table(sub9$pobre)
+write_csv(x = sub9,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/2.Entregables/Submission9.csv",)
+
+#####
+
+arbol3<-rpart(formula = as.formula(paste("Ingtotugarr~",
+                                                 paste(predictores_modelo, 
+                                                       collapse = " + "))),
+                      data=train_hogares_sin_bogota,
+                      parms = list(split="Gini"))
+
+prp(arbol3)
+
+train_hogares_sin_bogota$predic_ingreso_arbol3<-predict(arbol3,newdata =train_hogares_sin_bogota)
+
+train_hogares_sin_bogota$predict_arbol3<-ifelse(
+  train_hogares_sin_bogota$predic_ingreso_arbol3<
+    train_hogares_sin_bogota$Lp*train_hogares_sin_bogota$Npersug,
+  1,0)
+
+confusionMatrix(data = train_hogares_sin_bogota$predic_arbol2,
+                reference = train_hogares_sin_bogota$pobre_texto,
+                mode = "prec_recall")
+
+
+test_hogares$predic_ingreso_arbol3<-predict(arbol3,newdata =test_hogares)
+
+test_hogares$predict_arbol3<-ifelse(
+  test_hogares$predic_ingreso_arbol3<
+    test_hogares$Lp*test_hogares$Npersug,
+  1,0)
+
+table(test_hogares$predict_arbol3)
+
+sub10<-test_hogares %>% select(id,predict_arbol3)
+sub10<-sub10 %>% rename(pobre=predict_arbol3)
+table(test_hogares$predict_arbol3)
+table(sub10$pobre)
+write_csv(x = sub10,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/2.Entregables/Submission10.csv",)
+
+####
+
+
+predictores_modelo<-c("posicion_jefe",
+                      "Des_jefe",
+                      "Oc_jefe",
+                      "Pet_jefe",
+                      "ocupacion_jefe",
+                      "educacion_jefe",
+                      "regimen_jefe",
+                      "sexo_jefe",
+                      "Clase",
+                      "rural",
+                      "P5000",
+                      "P5010",
+                      "P5090",
+                      "P5100",
+                      "P5130",
+                      "P5140",
+                      "Nper",
+                      "Npersug",
+                      "jefe_menor",
+                      "Depto",
+                      "regimen_subsidiado_jefe",
+                      "desempleo_jefe",
+                      "Personas_habitacion_round"
+)
+
+
+arbol4<-rpart(formula = as.formula(paste("pobre_texto~",
+                                         paste(predictores_modelo, 
+                                               collapse = " * "))),
+              data=train_hogares_sin_bogota,
+              method = "class",
+              parms = list(split="Gini"))
+
+prp(arbol4)
+
+table(train_hogares_sin_bogota$Pobre,train_hogares_sin_bogota$regimen_subsidiado_jefe)
+train_hogares_sin_bogota$predic_arbol2<-predict(arbol2,newdata =train_hogares_sin_bogota,type = "class")
+
+confusionMatrix(data = train_hogares_sin_bogota$predic_arbol2,
+                reference = train_hogares_sin_bogota$pobre_texto,
+                positive="Pobre", mode = "prec_recall")
+
+test_hogares$predic_arbol2<-predict(arbol2,newdata =test_hogares,type = "class")
+table(test_hogares$predic_arbol2)
+
+sub9<-test_hogares %>% select(id,predic_arbol2)
+sub9<-sub9 %>% rename(pobre=predic_arbol2)
+sub9$pobre<-ifelse(sub9$pobre=="Pobre",1,0)
+table(test_hogares$predic_arbol2)
+table(sub9$pobre)
+write_csv(x = sub9,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/2.Entregables/Submission9.csv",)
+
