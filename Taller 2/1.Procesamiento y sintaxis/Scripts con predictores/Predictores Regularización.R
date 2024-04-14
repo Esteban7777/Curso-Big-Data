93d434672efbb28a59ff5645aa7e3ae6e3d32a89
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-04-13 19:43:38 -0500

    Create Predictores Regularización.R

---
+++ Taller 2/1.Procesamiento y sintaxis/Scripts con predictores/Predictores Regularización.R
@@ -0,0 +1,9 @@
+predictores<-c("desempleo_jefe", #Completa
+               "educacion_jefe", #Completa 
+               "sexo_jefe", #Completa
+               "Clase", #Completa
+               "P5090", #Completa
+               "Ina_jefe", #Completa 
+               "Des_jefe", #Completa
+               "Oc_jefe" #Completa
+)
