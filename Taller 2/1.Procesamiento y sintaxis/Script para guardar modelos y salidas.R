3852d5ca9e163c9764365da50211dabba0f1c65c
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-04-13 21:38:09 -0500

    Create Script para guardar modelos y salidas.R

---
+++ Taller 2/1.Procesamiento y sintaxis/Script para guardar modelos y salidas.R
@@ -0,0 +1,43 @@
+saveRDS(logit,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/modelo logit.rds")
+saveRDS(logit2,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/modelo logit con interacciones.rds")
+saveRDS(arbol,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/arbol.rds")
+
+saveRDS(RF,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/Ramdom Forest.rds")
+
+outputRF1000<-capture.output(RF1000)
+writeLines(outputRF1000,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/RF1000.txt")
+
+outputNODE100<-capture.output(RF_NODE100)
+writeLines(outputNODE100,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/RF_NODE100.txt")
+
+
+outputRF5<-capture.output(RF_5VAR$prediction.error)
+writeLines(outputRF5,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/RF_5VAR.txt")
+outputRF6<-capture.output(RF_6VAR$prediction.error)
+writeLines(outputRF6,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/RF_6VAR.txt")
+outputRF7<-capture.output(RF_7VAR$prediction.error)
+writeLines(outputRF7,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/RF_7VAR.txt")
+
+saveRDS(RF_5VAR,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/Ramdom Forest 5 VARIABLES.rds")
+
+outputlogit<-capture.output(stargazer(logit,type = "text"))
+writeLines(outputlogit,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/summary logit.txt")
+
+outputcf_logit<-capture.output(cf_logit)
+writeLines(outputcf_logit,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/outputcf_logit.txt")
+
+outputcf_arbol<-capture.output(cf_arbol)
+writeLines(outputcf_arbol,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/outputcf_arbol.txt")
+
+outputcf_rf<-capture.output(cf_rf)
+writeLines(outputcf_rf,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/outputcf_rf.txt")
+
+outputlm<-capture.output(stargazer(modelo_lm,type = "text"))
+writeLines(outputlm,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/summary lm.txt")
+
+outputcf_lm<-capture.output(cf_lm)
+writeLines(outputcf_lm,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/outputcf_lm.txt")
+
+outputcf_arbol_reg<-capture.output(cf_arbol_reg)
+writeLines(outputcf_arbol_reg,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 2/1.Procesamiento y sintaxis/outputcf_arbol_reg.txt")
+
