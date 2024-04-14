8bbca067ddf3fc8b089b7b808b9b2d06894d3687
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-04-13 19:38:25 -0500

    Create Predictores Regresión Líneal.R

---
+++ Taller 2/1.Procesamiento y sintaxis/Scripts con predictores/Predictores Regresión Líneal.R
@@ -0,0 +1,9 @@
+predictores<-c("desempleo_jefe",
+               "educacion_jefe", 
+               "sexo_jefe",
+               "Clase", 
+               "Dominio",  #Completa pero sin Bogotá en Test
+               "P5090", 
+               "Ina_jefe",
+               "Des_jefe",
+               "Oc_jefe")
