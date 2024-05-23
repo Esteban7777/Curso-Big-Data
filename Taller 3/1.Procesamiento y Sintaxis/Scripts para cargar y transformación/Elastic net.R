d6337228aeb3a30d10822ff18bafc2db7335a5d5
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-05-23 09:13:44 -0500

    Submit prediction with elastic net

--- Taller 3/1.Procesamiento y Sintaxis/Scripts para cargar y transformación/Elastic net.R
+++ Taller 3/1.Procesamiento y Sintaxis/Scripts para cargar y transformación/Elastic net.R
@@ -10,15 +10,21 @@ train<-read_csv("ruta del train")
 test<-read_csv("ruta del test")
 
 #Creamos los folds
-sectores<-unique(train$COD_Sector)
+sectores<-unique(train$cod_sector)
 folds<-createFolds(sectores,k=length(sectores),list = TRUE,returnTrain = TRUE)
 
 
 #Modelo 1
+#Modelo1
+# Variables predictoras
+predictores_modelo_en_1 <- c("nbanios", "nhabitaciones", "piso_apartamento", "estrato","Periodo")
 
-cv_modelo_en_1<-function(fold){
-  train_data<-datos %>% filter(COD_Sector %in% fold) %>% select(price, predictores_modelo_1)
-  test_data<- datos %>% filter(!COD_Sector %in% fold) %>% select(price, predictores_modelo_1)
+
+# Función de validación cruzada
+# Función de validación cruzada y ajuste de modelos en cada fold
+cv_modelo_en_1 <- function(fold) {
+  train_data <- train %>% filter(cod_sector %in% fold)  %>% select(price, predictores_modelo_en_1)
+  test_data <- train %>% filter(!cod_sector %in% fold)  %>% select(price, predictores_modelo_en_1)
   
   # Separar las características (X) y la variable objetivo (y)
   x_train <- model.matrix(price ~ . - 1, data = train_data)
@@ -26,77 +32,69 @@ cv_modelo_en_1<-function(fold){
   x_test <- model.matrix(price ~ . - 1, data = test_data)
   y_test <- test_data$price
   
-  # Entrenar el modelo de Elastic Net
-  cv_modelo <- cv.glmnet(x_train, y_train, alpha = 0.5)
   
-  #predicción fuera de muestra
-  prediccciones<-predict(modelo,newdata=test_data)
+  # Entrenar el modelo
+  modelo <- cv.glmnet(x = x_train, y = y_train, alpha = 0.5)
   
-  MAE<-mean(abs(train$price-predicciones))
+  # Hacer predicciones
+  predicciones <- predict(object = modelo, s=modelo$lambda.min ,newx = x_test)
   
-  return(MAE)
+  # Calcular MAE en el conjunto de prueba
+  MAE <- mean(abs(test_data$price - predicciones))
   
+  return(list(MAE = MAE, coef=as.vector(coef(modelo,s="lambda.min"))))
 }
 
-MAE<-mean(sapply(folds,cv_modelo_en_1))
+# Ejecutar validación cruzada y recolectar resultados
+resultados_cv_en_1 <- lapply(folds, function(fold) cv_modelo_en_1(train$cod_sector[fold]))
 
-#Modelo 2
+# Calcular el MAE promedio
+MAE_en_1 <- mean(sapply(resultados_cv_en_1, function(res) res$MAE))
+print(MAE_en_1)
 
-cv_modelo_en_2<-function(fold){
-  train_data<-datos %>% filter(COD_Sector %in% fold) %>% select(price, predictores_modelo_2)
-  test_data<- datos %>% filter(!COD_Sector %in% fold) %>% select(price, predictores_modelo_2)
-  
-  # Separar las características (X) y la variable objetivo (y)
-  x_train <- model.matrix(price ~ . - 1, data = train_data)
-  y_train <- train_data$price
-  x_test <- model.matrix(price ~ . - 1, data = test_data)
-  y_test <- test_data$price
-  
-  # Entrenar el modelo de Elastic Net
-  cv_modelo <- cv.glmnet(x_train, y_train, alpha = 0.5)
-  
-  #predicción fuera de muestra
-  prediccciones<-predict(modelo,newdata=test_data)
-  
-  MAE<-mean(abs(train$price-predicciones))
-  
-  return(MAE)
-  
-}
+coeficientes_en_1 <- do.call(rbind, lapply(resultados_cv_en_1, function(res) res$coef))
+coeficientes_promediados_en_1 <- colMeans(coeficientes_en_1)
 
-MAE<-mean(sapply(folds,cv_modelo_en_1))
+print(coeficientes_promediados_en_1)
 
-#Selección del mejor modelo
-min(MAE_en_1,MAE_en_2)
+x_full <- model.matrix(price ~ . - 1, data = train %>% select(price, all_of(predictores_modelo_en_1)))
+y_full <- train$price
 
-#Predicciones
+# Entrenar el modelo final Elastic Net con todos los datos
+modelo_final <- cv.glmnet(x = x_full, y = y_full, alpha = 0.5)
 
-modelo<-function(fold){
-  train_data<-datos %>% filter(COD_Sector %in% fold) %>% select(price, PREDICTORES DEL MEJOR MODELO)
-  test_data<- datos %>% filter(!COD_Sector %in% fold) %>% select(price, PREDICTORES DEL MEJOR MODELO)
-  
-  # Separar las características (X) y la variable objetivo (y)
-  x_train <- model.matrix(price ~ . - 1, data = train_data)
-  y_train <- train_data$price
-  x_test <- model.matrix(price ~ . - 1, data = test_data)
-  y_test <- test_data$price
-  
-  # Entrenar el modelo de Elastic Net
-  cv_modelo <- cv.glmnet(x_train, y_train, alpha = 0.5)
-  
-  #predicción fuera de muestra
-  prediccciones<-predict(modelo,newdata=test_data)
-  
-  MAE<-mean(abs(train$price-predicciones))
-  
-  return(modelo)
-  
-}
+# Imprimir los coeficientes del modelo final usando la lambda óptima
+coef_final <- coef(modelo_final, s = modelo_final$lambda.min)
+print(coef_final)
 
-test$precio_en<-predict(modelo,newdata=test)
+# Crear una nueva lista de coeficientes para el modelo final
+coef_final <- coef(modelo_final, s = 0.1)
+coef_final@x <- coeficientes_promediados
 
-sub_en<-test %>% select(id,precio_en)
-table(is.na(sub_en$precio_en))
-table(is.na(sub_en$id))
-      
-write_csv(x = sub_en,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 3/2.Entregables/Submission elastic net.csv",)
+# Imprimir el resumen del modelo final
+print(summary(modelo_final))
+
+#predecir sobre toda la muestra
+train$precio_en_1<-predict(modelo_final,newx = x_full)
+
+#MAE dentro de muestra
+MAE_en_1_insample<- mean(abs(train$price - train$precio_en_1))
+print(MAE_en_1_insample)
+
+#predecir fuera de muestra
+test<-test %>% rename(Periodo=Periodo.x)
+
+x_full_test <- test %>% select(predictores_modelo_en_1)
+x_full_test<-as.matrix(x_full_test)
+test$precio_en_1<-predict(modelo_final,newx = x_full_test)
+
+summary(test$precio_en_1)
+
+sub_en_1<-test %>% select(property_id,precio_en_1)
+
+table(is.na(sub_en_1$property_id))
+table(is.na(sub_en_1$precio_en_1))
+
+sub_en_1<-sub_en_1 %>% mutate(price=as.vector(precio_en_1))%>% select(property_id,price)
+
+write_csv(x = sub_en_1,"C:/Users/HP-Laptop/Documents/GitHub/Curso-Big-Data/Taller 3/2.Entregables/Submit/Submit_elastic_net_1.csv",)
